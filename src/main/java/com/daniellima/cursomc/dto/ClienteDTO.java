7114dc2185d908c6f186f3499b6b8094d8399399
Daniel Lima Lopes <daniel.lima.lopes@outlook.com>
2021-11-19 13:43:32 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/daniellima/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/daniellima/cursomc/dto/ClienteDTO.java
@@ -9,6 +9,7 @@ import org.hibernate.validator.constraints.Length;
 
 import com.daniellima.cursomc.domain.Cliente;
 
+
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
